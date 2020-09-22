06a027a3b5a33e6cbe0295415f25541e9fc0996f
kajamalie <69890213+kajamalie@users.noreply.github.com>
2020-09-22 18:43:58 +0200

    com_2

--- housedata.py
+++ housedata.py
@@ -4,6 +4,7 @@
 import os
 import numpy as np
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler
@@ -11,10 +12,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-
-import os
-import pandas as pd
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -63,9 +60,6 @@ X_filled = pd.DataFrame(np_train_features, columns=features_column_names)
 #Take this one further if you have missing values - I'm not using this one further 
 #now because I know that I have no missing values in X. 
 #%%
-#Look at the current X data:
-
-
 #Prep the data
 # log transformation
 X['sqft_living_log'] = np.log(X['sqft_living'])
@@ -99,7 +93,8 @@ preped_data_np = np.concatenate([X_no_scl, X_minmax, X_std, y_prep], axis=1)
 df_prep_data = pd.DataFrame(data =preped_data_np, columns=['view', 'waterfront','bedrooms', 'grade', 'lat', 'long', 'bathrooms', 'sqft_living_log', 'sqft_lot_log', 'price'])
 
 ############################################################################################################################################
-
+#%%
+#Split the data into train and test 
 df_train, df_test = train_test_split(df_prep_data, test_size = 0.2, random_state = 420)
 
 #create X and y (test and train)
@@ -114,30 +109,51 @@ y_train_arr = np.c_[train_y]
 X_test_arr = np.c_[test_X]
 y_test_arr = np.c_[test_y]
 
-input_layer = Input(shape=(9,))
-first_hidden_layer = Dense(9, activation = 'relu')(input_layer)
-second_hidden_layer = Dense(7, activation = 'relu')(first_hidden_layer)
-third_hidden_layer = Dense(6, activation = 'relu')(second_hidden_layer)
-output_layer = Dense(1, activation =None)(third_hidden_layer)
+#%%
+#Create the model and train it
 
+from tensorflow.keras.layers import Dense, Input, Dropout
+input_layer = Input(shape=(9,)) #Input shape 9 because there are 9 columns
+first_hidden_layer = Dense(10, activation = 'relu')(input_layer) #Dense in the layers may varry, but usually start similar to imput shape
+first_hidden_layer_d = Dropout(0.2)(first_hidden_layer)
+second_hidden_layer = Dense(15, activation = 'relu')(first_hidden_layer_d)
+second_hidden_layer_d = Dropout(0.2)(second_hidden_layer )
+third_hidden_layer = Dense(25, activation = 'relu')(second_hidden_layer_d)
+third_hidden_layer_d = Dropout(0.2)(third_hidden_layer)
+fourth_hidden_layer = Dense(15, activation = 'relu')(third_hidden_layer_d)
+output_layer = Dense(1, activation = None)(fourth_hidden_layer) #Output according to how many outputs(y) you want to predict
 #Combine the input and output into a model: 
-
 model_house = Model(inputs=input_layer, outputs = output_layer)
 model_house.compile(optimizer='adam', loss='mse', metrics=['mae'])
-model_house.fit(X_train_arr, y_train_arr, batch_size=64, epochs=50)
+
+#Make prediction
+house_history = model_house.fit(X_train_arr, y_train_arr, batch_size=64, epochs=70, validation_data =(X_test_arr, y_test_arr))
 
 
 
 y_train_pred = model_house.predict(X_train_arr )
 y_train_pred = y_train_pred.astype('float64')
 
-mean_absolute_error(y_train_arr, y_train_pred) #144189.56301318953 with 3p:50  143525.60292076346
-np.sqrt(mean_squared_error(y_train_arr, y_train_pred)) #236213.42797360278
+mean_absolute_error(y_train_arr, y_train_pred) #144189.56301318953 med dropout 70: 130178.37331646048, med dropout og 70:125353.18240447875, NEWEST: 121750.77959441874
+np.sqrt(mean_squared_error(y_train_arr, y_train_pred)) #236213.42797360278, 227747.68870825684(ep:50)
 
 
 y_test_pred = model_house.predict(X_test_arr )
-y_test_pred = y_test_pred.astype('float64')
+y_test_pred = y_test_pred.astype('float64')#make sure the pred has right format
 
-mean_absolute_error(y_test_arr, y_test_pred) #with 10 ep : 147736.17041298, with 50 ep:146763.61711159785
-np.sqrt(mean_squared_error(y_test_arr, y_test_pred)) #with 10 ep: 240723.12362202845
+mean_absolute_error(y_test_arr, y_test_pred) #with 10 ep : 147736.17041298, with 50 ep:146763.61711159785, 139663.14539418518, med dropout 70: 127247.595763666, NEWEST: 121715.27323328707
+np.sqrt(mean_squared_error(y_test_arr, y_test_pred)) #with 10 ep: 240723.12362202845, 231194.52588383618
+
+
+#%%%
+##Look at the graph for how the train and the test data is doing: 
+
+import matplotlib.pyplot as plt
+# make each plot seperatly 
+plt.plot(house_history.history['loss'], label='train loss')
+plt.plot(house_history.history['val_loss'], label='test loss')
+plt.legend(loc='upper right')
+plt.show()
+
+
 

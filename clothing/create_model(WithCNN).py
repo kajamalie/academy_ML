88973546c7868c833ecbd6dd7075cfe0250cc0ca
kajamalie <69890213+kajamalie@users.noreply.github.com>
2020-09-23 18:24:36 +0200

    2

--- clothing/create_model(WithCNN).py
+++ clothing/create_model(WithCNN).py
@@ -5,9 +5,6 @@ Created on Wed Sep 23 12:42:49 2020
 @author: Kaja Amalie
 """
 
-
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -21,7 +18,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 
-import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers import Dense, Input, Dropout
@@ -29,72 +25,82 @@ from tensorflow.keras.models import Model
 
 
 
-X_train =  np.load('train_images.npy', allow_pickle=True)
-y_train =  np.load('train_labels.npy', allow_pickle=True)
+X =  np.load('train_images.npy', allow_pickle=True)
+y =  np.load('train_labels.npy', allow_pickle=True)
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 # show image
 data_idx = 1
-plt.imshow(X_train[data_idx,:,:]/255, cmap='binary')
-class_number = y_train[data_idx]
+plt.imshow(X[data_idx,:,:]/255, cmap='binary')
+class_number = y[data_idx]
 class_text = class_names[class_number]
 print(f'This is a {class_text}')
 
 
 # data prep
-X_train = X_train/255
-X_train = X_train.reshape(-1, 784)
+X = X/255
+X = X.reshape(-1, 28,28, 1)
 
-y_train = y_train.reshape(-1, 1)
-y_train = y_train.astype('float64')
+y = y.reshape(-1, 1)
+y = y.astype('float64')
 
 #Hot Encoder for y
 from sklearn.preprocessing import OneHotEncoder
 clothing_ohe = OneHotEncoder(sparse=False)
-clothing_ohe.fit(y_train)
-y_train = clothing_ohe.transform(y_train)
-
+clothing_ohe.fit(y)
+y = clothing_ohe.transform(y)
+  
 
 #Split the data: 
-X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size = 0.2, random_state = 420)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 420)
 # create a model and train it:
 
 # prep validation data
 X_val =  np.load('val_images.npy', allow_pickle=True)
 X_val = X_val/255
 X_val = X_val.reshape(-1, 784)
-
+X_val = X_val.reshape(-1, 28,28, 1)
 #############################################################################################################
+#%% training the model
+from tensorflow.keras.layers import Dense, Input, Conv2D, MaxPool2D
+from tensorflow.keras.models import Model
+tf.keras.layers.BatchNormalization
+​
+input_layer = Input(shape=(28,28,1))
+first_conv = Conv2D(30, (3,3), activation='relu')(input_layer)
+pooling_layer = MaxPool2D (pool_size = (3,3))(first_conv)
+#second_conv  = Conv2D(25, (3,3), activation='relu')(pooling_layer)
+#pooling_layer_2 = MaxPool2D (pool_size = (3,3))(second_conv)
+flatten_layer = tf.keras.layers.Flatten()(pooling_layer)
+first_hidden_layer = Dense(20, activation='relu')(flatten_layer)
+second_hidden_layer = Dense(15, activation='relu')(first_hidden_layer)
+output_layer = Dense(10, activation='softmax')(second_hidden_layer)
 
-image_input = Input(shape=(784,))
-first_hidden_layer = Dense(114, activation='relu')(image_input)
-first_hidden_layer_d = Dropout(0.5)(first_hidden_layer)
-second_hidden_layer = Dense(256, activation='relu')(first_hidden_layer)
-second_hidden_layer_d = Dropout(0.2)(second_hidden_layer )
-third_hidden_layer = Dense(32, activation='relu')(second_hidden_layer_d)
-output_layer = Dense(10, activation='softmax')(third_hidden_layer)
 
 
-model_pic = Model(inputs = image_input, outputs=output_layer)
+
+model_pic = Model(inputs = input_layer, outputs=output_layer)
 model_pic.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 
 
-history_pic = model_pic.fit(X_train, y_train, batch_size=64, epochs=11, validation_data = (X_test, y_test))
+history_pic = model_pic.fit(X_train, y_train, batch_size=150, epochs=8, validation_data = (X_test, y_test))
 
 
 
 from sklearn.metrics import accuracy_score
 
-y_train_pred = model_pic.predict(X_train)
-accuracy_score(y_train, y_train_pred>0.5) # 0. 0.905827205882353, 0.9023713235294117, 0.9040625
+y_train_pred = model_pic.predict(X_train) #92,9
+accuracy_score(y_train, y_train_pred>0.5) 
 
 y_test_pred = model_pic.predict(X_test)
-accuracy_score(y_test, y_test_pred>0.5) # 0.905827205882353 , 0.8804411764705883, 0.8816911764705883
+accuracy_score(y_test, y_test_pred>0.5) 
+
+
 
 y_val_pred = model_pic.predict(X_val)
-y_val_pred =y_val_pred.astype('float64')
+y_val_pred =y_train_pred.astype('float64')
 
 pred1 = np.argmax(y_val_pred, axis=1)
 
@@ -104,7 +110,6 @@ pred1 = np.argmax(y_val_pred, axis=1)
 
 #%%
 
-
 import matplotlib.pyplot as plt
 # make each plot seperatly 
 plt.plot(history_pic.history['loss'], label='train loss')
@@ -112,15 +117,10 @@ plt.plot(history_pic.history['val_loss'], label='test loss')
 plt.legend(loc='upper right')
 plt.show()
 
+   
 
 
-image_input = Input(shape=(784,))
-first_hidden_layer = Dense(784, activation='relu')(image_input)
-first_hidden_layer_d = Dropout(0.5)(first_hidden_layer)
-second_hidden_layer = Dense(100, activation='relu')(first_hidden_layer)
-second_hidden_layer_d = Dropout(0.2)(second_hidden_layer )
-third_hidden_layer = Dense(300, activation='relu')(second_hidden_layer_d)
-output_layer = Dense(10, activation='softmax')(third_hidden_layer)
+
 
 
 
